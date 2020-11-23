77f968f3d088223c32172a56fc89c7f48def26d0
Eric <erichadley8@gmail.com>
2020-11-23 14:49:04 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+import random
+
+
+while True:
+
+	driver = webdriver.Firefox()
+	driver.get('http://www.porngifs.xyz/')
+
+	driver.switch_to.window(driver.window_handles[1])
+	driver.close()
+
+	driver.switch_to.window(driver.window_handles[0])
+
+	elements = driver.find_elements_by_class_name("card-img-top")
+	
+	clickthrough = random.choice(elements)
+
+	clickthrough.click()
+	driver.close()
